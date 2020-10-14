4211cdd5091cf02256fabc9be53f93ac37911e61
elik <elik@example.com>
2020-10-14 13:36:02 +0300

    Загрузка файлів

---
+++ BooksCollection/Classes/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksCollection
+{
+    class Book
+    {
+        public string title { get; set; }
+        public int strSize { get; set; }
+
+        public bool isGived { get; set; }
+        public int price { get; set; }
+
+        public Book(string title, int strSize, int price, bool isGived)
+        {
+            this.title = title;
+            this.strSize = strSize;
+            this.price = price;
+            this.isGived = isGived;
+        }
+
+        public void ShowBookInfo() 
+        {
+            Console.WriteLine("\n***");
+            Console.WriteLine("Назва: " + this.title);
+            Console.WriteLine("Кількість сторінок: " + this.strSize);
+            Console.WriteLine("Ціна: " + this.price);
+            Console.WriteLine("Чи видавалась цьогоріч: " + this.isGived);
+            Console.WriteLine("***\n");
+        }
+
+        
+
+    }
+}
---
+++ BooksCollection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BooksCollection
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            List<Book> books = new List<Book>();
+            books.Add(new Book("Кобзар", 1000, 100, true));
+            books.Add(new Book("Кайдашева сім'я", 200, 50, false));
+            books.Add(new Book("Гаррі Потер", 400, 500, false));
+            books.Add(new Book("Голодні ігри", 450, 300, true));
+
+            Console.WriteLine("1. Вибрати всі книжки вартість яких дорожче заданої");
+            while(true) 
+            {
+               try
+               {
+                    Console.Write("Ведіть ціну - ");
+                    int inPrice = int.Parse(Console.ReadLine());
+
+                    List<Book> books2 = books.Where(delegate (Book book) { return book.price > inPrice; }).ToList();
+
+                    books2.ForEach(book => { book.ShowBookInfo(); });
+                    break;
+               } 
+               catch (Exception ex) 
+               {
+                   Console.WriteLine(ex.Message);
+               }
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("2. Вибрати всі книжки які видавались за останній рік");
+            List<Book> books3 = books.Where((Book b) => { return b.isGived; }).ToList();
+            books3.ForEach(delegate (Book book) { book.ShowBookInfo(); });
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("3. На основі колекції книжок сформувати колекцію яка містить лише назви книжок");
+            List<string> booksStrings = books.Select(delegate (Book book) { return new string(book.title); } ).ToList();
+            Console.WriteLine("Назви книжок:");
+            foreach (var item in booksStrings) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
